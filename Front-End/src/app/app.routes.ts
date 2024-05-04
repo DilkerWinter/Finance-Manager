eb112f74a19a0c9dde537c093ab731e059eaa17a
Bruno Winter <brunohdwinter@gmail.com>
2024-05-03 22:34:11 -0400

    Create Page LoginScreen

--- Front-End/src/app/app.routes.ts
+++ Front-End/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:LoginScreenComponent},
+    {path: '*', redirectTo: '/'}
+];
