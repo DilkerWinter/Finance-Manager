10d0d8da32ca42b6dd39b49e2b86f1bbc106840d
Bruno Winter <brunohdwinter@gmail.com>
2024-05-04 12:29:54 -0400

    Add icon and first routes

--- Front-End/src/app/app.routes.ts
+++ Front-End/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
+import { FinanceManagerComponent } from './pages/finance-manager/finance-manager.component';
 
 export const routes: Routes = [
-    {path: '', component:LoginScreenComponent},
-    {path: '*', redirectTo: '/'}
+    { path: '', component: LoginScreenComponent }, 
+    { path: 'app', component: FinanceManagerComponent }, 
+    { path: '**', redirectTo: '/' } 
 ];
---
+++ Front-End/src/app/pages/finance-manager/finance-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finance-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './finance-manager.component.html',
+  styleUrl: './finance-manager.component.css'
+})
+export class FinanceManagerComponent {
+
+}
