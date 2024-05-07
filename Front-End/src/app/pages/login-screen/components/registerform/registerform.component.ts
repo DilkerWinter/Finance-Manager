26702ed7a1da7c72658e992e38372579873b6785
Bruno Winter <brunohdwinter@gmail.com>
2024-05-06 23:49:18 -0400

    register user create

--- Front-End/src/app/pages/login-screen/components/registerform/registerform.component.ts
+++ Front-End/src/app/pages/login-screen/components/registerform/registerform.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-registerform',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './registerform.component.html',
   styleUrl: './registerform.component.css'
 })
 export class RegisterformComponent {
+  faEnvelope = faEnvelope;
+  faLock = faLock;
+  faUser = faUser;
+  warning:string = "Please verify your credentials"
 
 }
