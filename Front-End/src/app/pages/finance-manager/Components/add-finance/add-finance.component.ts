df00e8be7eb7868fdbbcf29699f10a5e24647989
Bruno Winter <brunohdwinter@gmail.com>
2024-05-14 00:11:12 -0400

    Component created

---
+++ Front-End/src/app/pages/finance-manager/Components/add-finance/add-finance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-finance',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-finance.component.html',
+  styleUrl: './add-finance.component.css'
+})
+export class AddFinanceComponent {
+
+}
